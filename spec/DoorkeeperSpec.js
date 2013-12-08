93c6520f160bbde0ab30ea9fc725c053221ee041
grimrose <grwin7@gmail.com>
2013-12-08 17:43:58 +0900

    update for public

--- spec/DoorkeeperSpec.js
+++ spec/DoorkeeperSpec.js
@@ -1,23 +1,23 @@
-describe("Doorkeeper", function() {
-  var doorkeeper;
+describe("Doorkeeper", function () {
+    var doorkeeper;
 
-  beforeEach(function() {
-    doorkeeper = new Doorkeeper();
-    spyOn(Doorkeeper.prototype, "eventCallback");
-  });
+    beforeEach(function () {
+        doorkeeper = new Doorkeeper();
+        spyOn(Doorkeeper.prototype, "eventCallback");
+    });
 
-  it("doorkeeperオブジェクトが存在すること", function() {
-      expect(doorkeeper).not.toBeNull();
-  });
+    it("doorkeeperオブジェクトが存在すること", function () {
+        expect(doorkeeper).not.toBeNull();
+    });
 
-    it("コールバックが呼ばれること", function(){
+    it("コールバックが呼ばれること", function () {
         doorkeeper.search();
 
-        waitsFor(function(){
+        waitsFor(function () {
             return doorkeeper.eventCallback.calls.length > 0;
         });
 
-        runs(function(){
+        runs(function () {
             expect(doorkeeper.eventCallback.calls.length).toBeGreaterThan(1);
         });
     });
--- src/Doorkeeper.js
+++ src/Doorkeeper.js
@@ -1,17 +1,17 @@
 function Doorkeeper() {
 }
 
-Doorkeeper.prototype.search = function() {
+Doorkeeper.prototype.search = function () {
 
-    $.getJSON('http://api.doorkeeper.jp/events?callback=?', function(response) {
-        $.each(response, function(i, obj){
+    $.getJSON('http://api.doorkeeper.jp/events?callback=?', function (response) {
+        $.each(response, function (i, obj) {
             var event = obj.event;
             Doorkeeper.prototype.eventCallback(event);
         });
     });
 };
 
-Doorkeeper.prototype.eventCallback = function(event){
+Doorkeeper.prototype.eventCallback = function (event) {
     var tr = $("<tr/>");
     $("<td/>").text(event.title).appendTo(tr);
     $("<td/>").text(event.starts_at).appendTo(tr);
