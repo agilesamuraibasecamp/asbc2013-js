7e539555a518d8ffe75429418cddfd733359e087
grimrose <grwin7@gmail.com>
2013-12-08 14:31:54 +0900

    show doorkeeper event

--- src/Doorkeeper.js
+++ src/Doorkeeper.js
@@ -4,6 +4,13 @@ function Doorkeeper() {
 Doorkeeper.prototype.search = function() {
 
     $.getJSON('http://api.doorkeeper.jp/events?callback=?', function(response) {
-        console.log(response);
+        $.each(response, function(i, obj){
+            var event = obj.event;
+            var tr = $("<tr/>");
+            $("<td/>").text(event.title).appendTo(tr);
+            $("<td/>").text(event.starts_at).appendTo(tr);
+            $("<td/>").text(event.ends_at).appendTo(tr);
+            tr.appendTo($("#events"));
+        });
     });
 };
