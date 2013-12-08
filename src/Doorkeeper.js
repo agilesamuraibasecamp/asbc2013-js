14951d26db13161f50e97d7446ac33a0ee3a8aab
grimrose <grwin7@gmail.com>
2013-12-08 14:16:31 +0900

    WIP doorkeeper API

--- src/Doorkeeper.js
+++ src/Doorkeeper.js
@@ -2,4 +2,8 @@ function Doorkeeper() {
 }
 
 Doorkeeper.prototype.search = function() {
+
+    $.getJSON('http://api.doorkeeper.jp/events?callback=?', function(response) {
+        console.log(response);
+    });
 };
