9dc03c3d749e334bcdcd5fa5cc7d5faf39c383f1
grimrose <grwin7@gmail.com>
2013-12-08 14:45:50 +0900

    Add ajax test

--- spec/DoorkeeperSpec.js
+++ spec/DoorkeeperSpec.js
@@ -3,10 +3,23 @@ describe("Doorkeeper", function() {
 
   beforeEach(function() {
     doorkeeper = new Doorkeeper();
+    spyOn(Doorkeeper.prototype, "eventCallback");
   });
 
   it("doorkeeperオブジェクトが存在すること", function() {
       expect(doorkeeper).not.toBeNull();
   });
+
+    it("コールバックが呼ばれること", function(){
+        doorkeeper.search();
+
+        waitsFor(function(){
+            return doorkeeper.eventCallback.calls.length > 0;
+        });
+
+        runs(function(){
+            expect(doorkeeper.eventCallback.calls.length).toBeGreaterThan(1);
+        });
+    });
 
 });
--- src/Doorkeeper.js
+++ src/Doorkeeper.js
@@ -6,11 +6,15 @@ Doorkeeper.prototype.search = function() {
     $.getJSON('http://api.doorkeeper.jp/events?callback=?', function(response) {
         $.each(response, function(i, obj){
             var event = obj.event;
-            var tr = $("<tr/>");
-            $("<td/>").text(event.title).appendTo(tr);
-            $("<td/>").text(event.starts_at).appendTo(tr);
-            $("<td/>").text(event.ends_at).appendTo(tr);
-            tr.appendTo($("#events"));
+            Doorkeeper.prototype.eventCallback(event);
         });
     });
 };
+
+Doorkeeper.prototype.eventCallback = function(event){
+    var tr = $("<tr/>");
+    $("<td/>").text(event.title).appendTo(tr);
+    $("<td/>").text(event.starts_at).appendTo(tr);
+    $("<td/>").text(event.ends_at).appendTo(tr);
+    tr.appendTo($("#events"));
+}
